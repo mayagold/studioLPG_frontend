06bd956f58d03dc4462c3dae97d35607209ad096
mayagold <maya.gold.1991@gmail.com>
2017-11-07 22:04:46 -0800

    creating login form

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 var app = angular.module('studioLPG', []);
 
 app.controller('mainController', ['$http', function($http){
-  this.test = "Lisa Philipps Goldstein"
+
+  this.url = "http://localhost:3000";
+
+  this.user = {};
+
+
+  this.login = function(userPass) {
+    console.log(userPass);
+
+    $http({
+      method: 'POST',
+      url: this.url + '/users/login',
+      data: { user: { username: userPass.username, password: userPass.password } }
+    }).then(response => {
+      console.log(response);
+      this.user = response.data.user;
+    });
+  }
 }])

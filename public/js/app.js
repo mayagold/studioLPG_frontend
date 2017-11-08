6a97a03505a96c83bdb57eeb7c07b9324bef6d53
mayagold <maya.gold.1991@gmail.com>
2017-11-07 22:16:16 -0800

    JWT localStorage is set up

--- public/js/app.js
+++ public/js/app.js
@@ -7,9 +7,7 @@ app.controller('mainController', ['$http', function($http){
   this.user = {};
 
 
-  this.login = function(userPass) {
-    console.log(userPass);
-
+  this.login = (userPass) => {
     $http({
       method: 'POST',
       url: this.url + '/users/login',
@@ -17,6 +15,26 @@ app.controller('mainController', ['$http', function($http){
     }).then(response => {
       console.log(response);
       this.user = response.data.user;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+      console.log(localStorage);
+      this.getUser();
     });
+  }
+
+  this.getUser = () => {
+    $http({
+      url: this.url + '/users',
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+      }
+    }).then( response => {
+      console.log(response);
+      if (response.data.status === 401) {
+        this.error = "Unauthorized";
+      } else {
+        this.loggedIn = true;
+      }
+    })
   }
 }])
